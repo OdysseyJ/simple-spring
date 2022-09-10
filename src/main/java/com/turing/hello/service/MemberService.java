a4115b5de3dd30ab0efd04a439ac9103d44d4f63
seongwoon jeong <ggogumalove@gmail.com>
2022-09-10 23:14:39 +0900

    [feat] member services

---
+++ src/main/java/com/turing/hello/service/MemberService.java
@@ -0,0 +1,33 @@
+package com.turing.hello.service;
+
+import com.turing.hello.domain.Member;
+import com.turing.hello.repository.MemberRepository;
+import com.turing.hello.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member){
+        validateDuplicatedMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicatedMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원");
+        });
+    }
+
+    public List<Member> findMembers(){
+       return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
