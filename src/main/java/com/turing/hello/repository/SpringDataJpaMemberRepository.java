d6602960e6c2205edf12229ca6710c6b55d40ccd
seongwoon jeong <ggogumalove@gmail.com>
2022-09-12 21:50:18 +0900

    [feat] add spring data jpa

---
+++ src/main/java/com/turing/hello/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,12 @@
+package com.turing.hello.repository;
+
+import com.turing.hello.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
