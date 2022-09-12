01256275b4ab12caf2aa6fd23af610be677ba615
seongwoon jeong <ggogumalove@gmail.com>
2022-09-12 20:46:01 +0900

    [feat] study 9/12

--- src/main/java/com/turing/hello/controller/MemberController.java
+++ src/main/java/com/turing/hello/controller/MemberController.java
@@ -3,14 +3,15 @@ package com.turing.hello.controller;
 import com.turing.hello.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
+
+    private final MemberService memberService;
 
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
-
-    private final MemberService memberService;
 }
---
+++ src/test/java/com/turing/hello/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,34 @@
+package com.turing.hello.service;
+
+import com.turing.hello.domain.Member;
+import com.turing.hello.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class MemberServiceIntegrationTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+}
